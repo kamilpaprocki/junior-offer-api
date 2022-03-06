670122ea137603987b16c1d355fe308c72ca6306
Kamil Paprocki <kamil.paprocki1@gmail.com>
2022-03-06 17:40:07 +0100

    added main method to run application

--- src/main/java/com/junioroffers/JobOffersApplication.java
+++ src/main/java/com/junioroffers/JobOffersApplication.java
@@ -1,7 +1,11 @@
 package com.junioroffers;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobOffersApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JobOffersApplication.class, args);
+    }
 }
