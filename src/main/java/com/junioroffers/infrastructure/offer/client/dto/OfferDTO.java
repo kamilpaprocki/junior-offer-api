67555e7537a9e60fb60466da73f105d07113e138
Kamil Paprocki <kamil.paprocki1@gmail.com>
2022-03-15 16:29:34 +0100

    fixed mapping model from JSON

--- src/main/java/com/junioroffers/infrastructure/offer/client/dto/OfferDTO.java
+++ src/main/java/com/junioroffers/infrastructure/offer/client/dto/OfferDTO.java
@@ -1,5 +1,6 @@
 package com.junioroffers.infrastructure.offer.client.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 @Data
@@ -7,9 +8,13 @@ import lombok.*;
 public class OfferDTO {
 
     private String id;
+    @JsonProperty("company")
     private String companyName;
+    @JsonProperty("title")
     private String jobPosition;
+    @JsonProperty("salary")
     private String salary;
+    @JsonProperty("offerUrl")
     private String offerUrl;
 
 }
