652bdd73e70a250018f717bede2914b1d15b9d4c
Kamil Paprocki <kamil.paprocki1@gmail.com>
2022-03-28 19:48:19 +0200

    added logger to http offer client

--- src/main/java/com/junioroffers/infrastructure/offer/client/OfferClient.java
+++ src/main/java/com/junioroffers/infrastructure/offer/client/OfferClient.java
@@ -4,12 +4,14 @@ import com.junioroffers.infrastructure.offer.client.dto.OfferDto;
 import com.junioroffers.infrastructure.offer.client.exceptions.HttpClientException;
 import com.junioroffers.infrastructure.offer.client.exceptions.OfferNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import java.util.List;
 
+@Slf4j
 @Component
 @AllArgsConstructor
 public class OfferClient implements RemoteOfferClient {
@@ -23,11 +25,14 @@ public class OfferClient implements RemoteOfferClient {
             });
             offers = responseEntity.getBody();
             if (offers != null && offers.isEmpty()) {
+                log.error("Return empty offer list.");
                 throw new OfferNotFoundException("There are no offers");
             }
         } catch (RestClientException e) {
+            log.error("Throw RestClient exception with message {}.", e.getMessage());
             throw new HttpClientException(e.getMessage());
         }
+        log.info("Return list with {} offers.", offers.size());
         return offers;
     }
 }
