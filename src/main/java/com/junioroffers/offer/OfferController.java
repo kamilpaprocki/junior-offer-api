b5097036321de5de46fdfed785eb95aa122e6807
Kamil Paprocki <kamil.paprocki1@gmail.com>
2022-03-28 19:43:13 +0200

    added logger to rest controller

--- src/main/java/com/junioroffers/offer/OfferController.java
+++ src/main/java/com/junioroffers/offer/OfferController.java
@@ -3,11 +3,13 @@ package com.junioroffers.offer;
 import com.junioroffers.offer.domain.dto.OfferDto;
 import com.junioroffers.offer.domain.exceptions.OfferNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
+@Slf4j
 @RestController
 @AllArgsConstructor
 public class OfferController {
@@ -18,8 +20,10 @@ public class OfferController {
     public ResponseEntity<List<OfferDto>> getOffers() {
         List<OfferDto> offers = offerService.getOffers();
         if (offers.isEmpty()) {
+            log.error("List of offers is empty.");
             throw new OfferNotFoundException("There are no offers");
         }
+        log.info("Return {} offers.", offers.size());
         return new ResponseEntity<>(offers, HttpStatus.OK);
     }
 
@@ -27,24 +31,30 @@ public class OfferController {
     public ResponseEntity<OfferDto> getOfferById(@PathVariable Long id) {
         OfferDto offer = offerService.getOfferById(id);
         if (offer == null) {
+            log.error("Offer with id {} not found.", id);
             throw new OfferNotFoundException("There is no offer with id: " + id);
         }
+        log.info("Return offer with id {}. ", id);
         return new ResponseEntity<>(offer, HttpStatus.OK);
     }
 
     @PostMapping("/offers")
     public ResponseEntity<OfferDto> createOrUpdateOffer(@RequestBody OfferDto offerDto) {
         if (offerDto.getId() != null) {
+            log.info("Update offer with id {}. ", offerDto.getId());
             return new ResponseEntity<>(offerService.createOrUpdateOffer(offerDto), HttpStatus.OK);
         }
+        log.info("Added new offer");
         return new ResponseEntity<>(offerService.createOrUpdateOffer(offerDto), HttpStatus.CREATED);
     }
 
     @DeleteMapping("/offers/{id}")
     public ResponseEntity<OfferDto> deleteOffer(@PathVariable Long id) {
         if (offerService.deleteOfferById(id) > 0) {
+            log.info("Delete offer with id {}.", id);
             return new ResponseEntity<>(HttpStatus.OK);
         }
+        log.info("Offer with id {} do not exists.", id);
         return new ResponseEntity<>(HttpStatus.NOT_MODIFIED);
     }
 }
